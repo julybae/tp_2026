34bda02e5608b259b0c45e463e2a372f8e6165cb
a-sik <andreyskor07@gmail.com>
2026-06-04 22:40:01 +0300

    Fixed content 2

--- skorohod.andrey/T2/main.cpp
+++ skorohod.andrey/T2/main.cpp
@@ -19,14 +19,10 @@ int main()
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       continue;
     }
-    std::copy(std::istream_iterator<DataStruct>(std::cin),
-      std::istream_iterator<DataStruct>(),
-      std::back_inserter(data));
+    std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(data));
   }
   std::sort(data.begin(), data.end());
   std::cout << "DataStruct:\n";
-  std::copy(std::begin(data),
-    std::end(data),
-    std::ostream_iterator< DataStruct >(std::cout, "\n"));
+  std::copy(std::begin(data), std::end(data), std::ostream_iterator< DataStruct >(std::cout, "\n"));
   return 0;
 }
