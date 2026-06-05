fb372a1031bbb84a26c813116de4279689630af5
mrchudak <gleb.ts2104@gmail.com>
2026-06-05 17:38:17 +0300

    fix content

---
+++ tsuremichev.gleb/T3/commands.hpp
@@ -0,0 +1,9 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <vector>
+#include "polygon.hpp"
+
+void processCommands(std::vector<Polygon> &shapes);
+
+#endif
--- tsuremichev.gleb/T3/geometry.cpp
+++ tsuremichev.gleb/T3/geometry.cpp
@@ -9,14 +9,16 @@ double getArea(const Polygon &poly)
     return 0.0;
   double area = 0.0;
   size_t n = poly.points.size();
-  const Point *base = poly.points.data(); // Базовый адрес начала массива точек
+  const Point *base = poly.points.data();
 
   area = std::accumulate(poly.points.begin(), poly.points.end(), 0.0,
                          [&poly, n, base](double sum, const Point &p)
                          {
-                           size_t idx = &p - base; // Теперь типы совпадают: (const Point*) - (const Point*)
-                           size_t next_idx = (idx + 1) % n;
-                           return sum + (poly.points[idx].x * poly.points[next_idx].y) - (poly.points[next_idx].x * poly.points[idx].y);
+                    size_t idx = &p - base;
+                    size_t next_idx = (idx + 1) % n;
+                    return sum +
+                    (poly.points[idx].x * poly.points[next_idx].y) -
+                    (poly.points[next_idx].x * poly.points[idx].y);
                          });
 
   return std::abs(area) / 2.0;
@@ -26,7 +28,10 @@ bool isIntersectingSegments(Point a, Point b, Point c, Point d)
 {
   auto ccw = [](Point p1, Point p2, Point p3)
   {
-    long long val = 1LL * (p3.y - p1.y) * (p2.x - p1.x) - 1LL * (p2.y - p1.y) * (p3.x - p1.x);
+    long long val = 1LL * (p3.y - p1.y) *
+                        (p2.x - p1.x) -
+                    1LL *
+                        (p2.y - p1.y) * (p3.x - p1.x);
     return (val > 0) ? 1 : ((val < 0) ? -1 : 0);
   };
 
