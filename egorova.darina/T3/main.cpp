96c8aed4872bc73b22d9e7addc5cd7da24ff7b64
egorova.darina <motobikerdrag@yandex.ru>
2026-06-06 15:07:35 +0300

    Initial commit for T3 (11)

--- egorova.darina/T3/main.cpp
+++ egorova.darina/T3/main.cpp
@@ -2,33 +2,56 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <map>
+#include <functional>
 #include "Polygon.h"
 #include "Commands.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) return 1;
-    std::ifstream file(argv[1]); if (!file) return 1;
-    std::vector<Polygon> figures; std::string line;
+    std::ifstream file(argv[1]);
+    if (!file) return 1;
+
+    std::vector<Polygon> figures;
+    std::string line;
     while (std::getline(file, line)) {
         if (line.empty()) continue;
-        std::stringstream ss(line); Polygon poly;
+        std::stringstream ss(line);
+        Polygon poly;
         if (ss >> poly) {
-            std::string extra; if (!(ss >> extra)) figures.push_back(poly);
+            std::string extra;
+            if (!(ss >> extra)) figures.push_back(poly);
         }
     }
+
+    std::stringstream ss;
+    std::map<std::string, std::function<void(void)>> cmds = {
+        {"AREA", [&]() { cmdArea(figures, ss); }},
+        {"MAX", [&]() { cmdMax(figures, ss); }},
+        {"MIN", [&]() { cmdMin(figures, ss); }},
+        {"COUNT", [&]() { cmdCount(figures, ss); }},
+        {"INTERSECTIONS", [&]() { cmdIntersections(figures, ss); }},
+        {"RMECHO", [&]() { cmdRmEcho(figures, ss); }}
+    };
+
     while (std::getline(std::cin, line)) {
         if (line.empty()) continue;
-        std::stringstream ss(line); std::string cmd;
-        if (!(ss >> cmd)) continue;
+        ss.clear();
+        ss.str(line);
+        std::string cmdName;
+        if (!(ss >> cmdName)) continue;
+
         try {
-            if (cmd == "AREA") cmdArea(figures, ss);
-            else if (cmd == "INTERSECTIONS") cmdIntersections(figures, ss);
-            else if (cmd == "RMECHO") cmdRmEcho(figures, ss);
-            else if (cmd == "COUNT") cmdCount(figures, ss);
-            else if (cmd == "MAX") cmdMax(figures, ss);
-            else if (cmd == "MIN") cmdMin(figures, ss);
-            else throw std::runtime_error("");
-        } catch (...) { std::cout << "<INVALID COMMAND>\n"; }
+            auto it = cmds.find(cmdName);
+            if (it != cmds.end()) {
+                it->second();
+            } else {
+                throw std::runtime_error("");
+            }
+        } catch (...) {
+            std::cout << "<INVALID COMMAND>\n";
+        }
     }
+
     return 0;
 }
